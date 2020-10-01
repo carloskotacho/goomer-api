e9786da80359b886830c721d3e073b7de1680556
Carlos - PC <carloskotachoamd@hotmail.com>
2020-10-01 15:25:33 -0300

    chore: add migration for schedules

---
+++ src/database/migrations/20201001180921-create-schedules.js
@@ -0,0 +1,40 @@
+module.exports = {
+  up: (queryInterface, Sequelize) =>
+    queryInterface.createTable('schedules', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      restaurant_id: {
+        type: Sequelize.INTEGER,
+        references: { model: 'restaurants', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+        allowNull: true,
+      },
+      day_week: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      open_hours: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      closed_hours: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    }),
+
+  down: (queryInterface) => queryInterface.dropTable('schedules'),
+};
