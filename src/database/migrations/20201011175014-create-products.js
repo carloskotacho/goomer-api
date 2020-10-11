6f41142fc5ca13b407854fea70b71db9394497e1
Carlos - Notebook <carloskotachoamd@hotmail.com>
2020-10-11 18:52:32 -0300

    feat: add migration for products

---
+++ src/database/migrations/20201011175014-create-products.js
@@ -0,0 +1,52 @@
+module.exports = {
+  up: (queryInterface, Sequelize) =>
+    queryInterface.createTable('products', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      restaurant_id: {
+        type: Sequelize.INTEGER,
+        references: { model: 'restaurants', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+        allowNull: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      price: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      category: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      day_week: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      promotion_schedules: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    }),
+
+  down: (queryInterface) => queryInterface.dropTable('products'),
+};
