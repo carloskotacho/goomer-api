9eefe96a6b6c77c8f6e0447071532fb598d2774c
Carlos - Notebook <carloskotachoamd@hotmail.com>
2020-10-11 19:41:45 -0300

    feat: add products list by restaurant

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -16,6 +16,40 @@ class ProductController {
 
     return res.json(products);
   }
+
+  async show(req, res) {
+    const restaurantId = req.params.id;
+
+    const restaurant = await Restaurant.findByPk(restaurantId, {
+      attributes: {
+        exclude: ['createdAt', 'updatedAt'],
+      },
+    });
+
+    if (!restaurant) {
+      return res.status(404).json({ error: 'Restaurant not found' });
+    }
+
+    const products = await Product.findAll({
+      where: {
+        id: restaurantId,
+      },
+      attributes: {
+        exclude: ['restaurant_id', 'createdAt', 'updatedAt'],
+      },
+      include: {
+        model: Restaurant,
+        as: 'restaurant',
+        attributes: ['id', 'name'],
+      },
+    });
+
+    if (!products) {
+      return res.status(404).json({ error: 'Products not found' });
+    }
+
+    return res.json(products);
+  }
 }
 
 export default new ProductController();
