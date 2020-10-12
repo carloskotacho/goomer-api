3cddbc7f6c4c8e033bf3949850a8f8d539aec3cd
Carlos - Notebook <carloskotachoamd@hotmail.com>
2020-10-12 09:58:41 -0300

    feat: add validation for update product

---
+++ src/app/validators/ProductUpdate.js
@@ -0,0 +1,33 @@
+import * as Yup from 'yup';
+
+export default async (req, res, next) => {
+  try {
+    const schema = Yup.object().shape({
+      restaurant_id: Yup.number(),
+      name: Yup.string(),
+      price: Yup.string(),
+      category: Yup.string(),
+      promotion: Yup.boolean(),
+      description: Yup.string().when('promotion', {
+        is: true,
+        then: Yup.string().required(),
+      }),
+      day_week: Yup.array(Yup.string()).when('promotion', {
+        is: true,
+        then: Yup.array(Yup.string()).required(),
+      }),
+      promotion_schedules: Yup.array(Yup.string()).when('promotion', {
+        is: true,
+        then: Yup.array(Yup.string()).required(),
+      }),
+    });
+
+    await schema.validate(req.body, { abortEarly: false });
+
+    return next();
+  } catch (err) {
+    return res
+      .status(400)
+      .json({ error: 'Validations fails', messages: err.inner });
+  }
+};
