3115f4419fadea755c01e4b4cefadcc427d47419
Carlos - Notebook <carloskotachoamd@hotmail.com>
2020-10-12 16:49:44 -0300

    feat: add associate file

--- src/app/models/Restaurant.js
+++ src/app/models/Restaurant.js
@@ -15,6 +15,10 @@ class Restaurant extends Model {
 
     return this;
   }
+
+  static associate(models) {
+    this.belongsTo(models.File, { foreignKey: 'avatar_id' });
+  }
 }
 
 export default Restaurant;
