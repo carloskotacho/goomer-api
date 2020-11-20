1d4b17cf3193abd66e5e15af3e39da9176e34f2d
Carlos - PC <carloskotachoamd@hotmail.com>
2020-11-20 16:07:09 -0300

    feat: add update validator for restaurant

---
+++ src/app/validators/RestaurantUpdate.js
@@ -0,0 +1,22 @@
+import * as Yup from 'yup';
+
+export default async (req, res, next) => {
+  try {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      address: Yup.string(),
+      week_opening_time: Yup.string(),
+      week_closing_time: Yup.string(),
+      weekend_opening_time: Yup.string(),
+      weekend_closing_time: Yup.string(),
+    });
+
+    await schema.validate(req.body, { abortEarly: false });
+
+    return next();
+  } catch (err) {
+    return res
+      .status(400)
+      .json({ error: 'Validations fails', messages: err.inner });
+  }
+};
